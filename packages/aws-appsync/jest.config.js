5ce1b43941e341e5986b82d3b353936402395a75
Volkan Unsal <spocksplanet@gmail.com>
2021-02-27 16:08:32 -0500

    Mark progress

--- packages/aws-appsync/jest.config.js
+++ packages/aws-appsync/jest.config.js
@@ -1,9 +1,19 @@
+const esModules = ['@apollo/.*'].join('|');
+
 module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'ts-jest',
   },
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   collectCoverageFrom: ['src/**/*', '!src/vendor/**'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testEnvironment: 'node',
+  preset: 'ts-jest/presets/js-with-ts',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
 };
