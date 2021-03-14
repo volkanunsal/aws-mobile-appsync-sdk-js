b151fbac0e239cbddfa5013742a5a9e5543a716f
Volkan Unsal <spocksplanet@gmail.com>
2021-03-14 09:41:55 -0400

    Clean up the client

--- packages/aws-appsync/src/index.ts
+++ packages/aws-appsync/src/index.ts
@@ -2,7 +2,6 @@ import 'setimmediate';
 import {
   ApolloClient,
   ApolloClientOptions,
-  MutationOptions,
   OperationVariables,
   MutationUpdaterFn,
   ApolloCache,
@@ -12,70 +11,13 @@ import {
   ApolloReducerConfig,
   NormalizedCacheObject,
 } from '@apollo/client/cache';
-import { ApolloLink, NextLink } from '@apollo/client/link/core';
-import { Observable } from '@apollo/client/utilities';
+import { ApolloLink } from '@apollo/client/link/core';
 import { createHttpLink } from '@apollo/client/link/http';
-import {
-  AuthOptions,
-  AuthLink,
-  AUTH_TYPE,
-} from '@volkanunsal/aws-appsync-auth-link';
+import { AuthOptions, AUTH_TYPE } from '@volkanunsal/aws-appsync-auth-link';
 import { createSubscriptionHandshakeLink } from '@volkanunsal/aws-appsync-subscription-link';
 import { Credentials, CredentialProvider } from '@aws-sdk/types';
 import { DocumentNode } from 'graphql';
-import { passthroughLink } from './utils';
-import { createRetryLink } from './link/retry-link';
-import { PERMANENT_ERROR_KEY } from './link/retry-link';
 import { createAuthLink } from '@volkanunsal/aws-appsync-auth-link';
-
-const KEY_PREFIX = 'persist:';
-
-class CatchErrorLink extends ApolloLink {
-  private link: ApolloLink;
-
-  constructor(linkGenerator: () => ApolloLink) {
-    try {
-      super();
-      this.link = linkGenerator();
-    } catch (error) {
-      error[PERMANENT_ERROR_KEY] = true;
-      throw error;
-    }
-  }
-
-  request(operation, forward?: NextLink) {
-    return this.link.request(operation, forward);
-  }
-}
-
-class PermanentErrorLink extends ApolloLink {
-  private link: ApolloLink;
-
-  constructor(link: ApolloLink) {
-    super();
-
-    this.link = link;
-  }
-
-  request(operation, forward?: NextLink) {
-    return new Observable((observer) => {
-      const subscription = this.link.request(operation, forward).subscribe({
-        next: observer.next.bind(observer),
-        error: (err) => {
-          if (err.permanent) {
-            err[PERMANENT_ERROR_KEY] = true;
-          }
-          observer.error.call(observer, err);
-        },
-        complete: observer.complete.bind(observer),
-      });
-
-      return () => {
-        subscription.unsubscribe();
-      };
-    });
-  }
-}
 
 export const createAppSyncLink = ({
   url,
@@ -92,23 +34,10 @@ export const createAppSyncLink = ({
 }) => {
   const httpLink = createHttpLink({ uri: url });
 
-  const retryLink: ApolloLink = createRetryLink(
-    ApolloLink.from([
-      new CatchErrorLink(() => new AuthLink({ url, region, auth })),
-      new PermanentErrorLink(
-        createSubscriptionHandshakeLink(
-          { url, region, auth },
-          resultsFetcherLink
-        )
-      ),
-    ])
-  );
-
   return ApolloLink.from(
     [
       createAuthLink({ url, region, auth }),
       createSubscriptionHandshakeLink(url, httpLink),
-      retryLink,
     ].filter(Boolean)
   );
 };
@@ -121,25 +50,11 @@ export interface AWSAppSyncClientOptions {
   auth: AuthOptions;
   complexObjectsCredentials?: CredentialsGetter;
   cacheOptions?: ApolloReducerConfig;
-  disableOffline?: boolean;
 }
-
-// TODO: type defs
-export type OfflineCallback = (err: any, success: any) => void;
-
-const keyPrefixesInUse = new Set<string>();
 
 class AWSAppSyncClient<
   TCacheShape extends NormalizedCacheObject
 > extends ApolloClient<TCacheShape> {
-  private hydratedPromise: Promise<AWSAppSyncClient<TCacheShape>>;
-
-  hydrated() {
-    return this.hydratedPromise;
-  }
-
-  private _disableOffline: boolean;
-
   constructor(
     {
       url,
@@ -147,7 +62,6 @@ class AWSAppSyncClient<
       auth,
       complexObjectsCredentials,
       cacheOptions = {},
-      disableOffline = false,
     }: AWSAppSyncClientOptions,
     options?: Partial<ApolloClientOptions<NormalizedCacheObject>>
   ) {
@@ -160,36 +74,12 @@ class AWSAppSyncClient<
       );
     }
 
-    let keyPrefix = KEY_PREFIX;
-    if (!disableOffline && keyPrefixesInUse.has(keyPrefix)) {
-      throw new Error(
-        `The keyPrefix ${keyPrefix} is already in use. Multiple clients cannot share the same keyPrefix.`
-      );
-    }
-
     const inMemoryCache = new InMemoryCache(cacheOptions);
 
     const cache: ApolloCache<NormalizedCacheObject> =
       customCache || inMemoryCache;
 
-    const waitForRehydrationLink = new ApolloLink((op, forward) => {
-      let handle = null;
-
-      return new Observable((observer) => {
-        this.hydratedPromise
-          .then(() => {
-            handle = passthroughLink(op, forward).subscribe(observer);
-          })
-          .catch(observer.error);
-
-        return () => {
-          if (handle) {
-            handle.unsubscribe();
-          }
-        };
-      });
-    });
-    const apolloLink =
+    const link =
       customLink ||
       createAppSyncLink({
         url,
@@ -197,7 +87,6 @@ class AWSAppSyncClient<
         auth,
         complexObjectsCredentials,
       });
-    const link = waitForRehydrationLink.concat(apolloLink as ApolloLink);
 
     const newOptions: ApolloClientOptions<any> = {
       ...options,
@@ -206,52 +95,6 @@ class AWSAppSyncClient<
     };
 
     super(newOptions);
-
-    this.hydratedPromise = Promise.resolve(this);
-    this._disableOffline = disableOffline;
-
-    if (!disableOffline) {
-      keyPrefixesInUse.add(keyPrefix);
-    }
-  }
-
-  isOfflineEnabled() {
-    return !this._disableOffline;
-  }
-
-  mutate<T, TVariables = OperationVariables>(
-    options: MutationOptions<T, TVariables>
-  ) {
-    if (!this.isOfflineEnabled()) {
-      return super.mutate(options);
-    }
-
-    const doIt = false;
-    const {
-      context: origContext,
-      optimisticResponse,
-      update,
-      fetchPolicy,
-      ...otherOptions
-    } = options;
-
-    const context = {
-      ...origContext,
-      AASContext: {
-        doIt,
-        optimisticResponse,
-        update,
-        fetchPolicy,
-      },
-    };
-
-    return super.mutate({
-      optimisticResponse,
-      context,
-      update,
-      fetchPolicy,
-      ...otherOptions,
-    });
   }
 }
 
