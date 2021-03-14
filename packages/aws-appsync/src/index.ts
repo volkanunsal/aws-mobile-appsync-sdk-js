6d25247c5fe96812460d47f5826b009e532666d4
Volkan Unsal <spocksplanet@gmail.com>
2021-03-14 07:53:59 -0400

    Delete react repo

--- packages/aws-appsync/src/index.ts
+++ packages/aws-appsync/src/index.ts
@@ -2,8 +2,5 @@
  * Copyright 2017-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-import AWSAppSyncClient from './client';
 export * from './client';
 export { Signer } from '@volkanunsal/aws-appsync-auth-link';
-
-export default AWSAppSyncClient;
