6d9c66f7ecd673070151b1517996471a2a251c81
Volkan Unsal <spocksplanet@gmail.com>
2021-03-14 10:12:57 -0400

    Clean up

--- packages/aws-appsync/src/index.ts
+++ packages/aws-appsync/src/index.ts
@@ -1,11 +1,4 @@
-import 'setimmediate';
-import {
-  ApolloClient,
-  ApolloClientOptions,
-  OperationVariables,
-  MutationUpdaterFn,
-  ApolloCache,
-} from '@apollo/client';
+import { ApolloClient, ApolloClientOptions, ApolloCache } from '@apollo/client';
 import {
   InMemoryCache,
   ApolloReducerConfig,
@@ -15,40 +8,12 @@ import { ApolloLink } from '@apollo/client/link/core';
 import { createHttpLink } from '@apollo/client/link/http';
 import { AuthOptions, AUTH_TYPE } from '@volkanunsal/aws-appsync-auth-link';
 import { createSubscriptionHandshakeLink } from '@volkanunsal/aws-appsync-subscription-link';
-import { Credentials, CredentialProvider } from '@aws-sdk/types';
-import { DocumentNode } from 'graphql';
 import { createAuthLink } from '@volkanunsal/aws-appsync-auth-link';
-
-export const createAppSyncLink = ({
-  url,
-  region,
-  auth,
-  complexObjectsCredentials,
-  resultsFetcherLink = createHttpLink({ uri: url }),
-}: {
-  url: string;
-  region: string;
-  auth: AuthOptions;
-  complexObjectsCredentials: CredentialsGetter;
-  resultsFetcherLink?: ApolloLink;
-}) => {
-  const httpLink = createHttpLink({ uri: url });
-
-  return ApolloLink.from(
-    [
-      createAuthLink({ url, region, auth }),
-      createSubscriptionHandshakeLink(url, httpLink),
-    ].filter(Boolean)
-  );
-};
-
-type CredentialsGetter = () => Credentials | CredentialProvider | null;
 
 export interface AWSAppSyncClientOptions {
   url: string;
   region: string;
   auth: AuthOptions;
-  complexObjectsCredentials?: CredentialsGetter;
   cacheOptions?: ApolloReducerConfig;
 }
 
@@ -56,66 +21,25 @@ class AWSAppSyncClient<
   TCacheShape extends NormalizedCacheObject
 > extends ApolloClient<TCacheShape> {
   constructor(
-    {
-      url,
-      region,
-      auth,
-      complexObjectsCredentials,
-      cacheOptions = {},
-    }: AWSAppSyncClientOptions,
+    { url, region, auth, cacheOptions = {} }: AWSAppSyncClientOptions,
     options?: Partial<ApolloClientOptions<NormalizedCacheObject>>
   ) {
-    const { cache: customCache = undefined, link: customLink = undefined } =
-      options || {};
-
-    if (!customLink && (!url || !region || !auth)) {
-      throw new Error(
-        'In order to initialize AWSAppSyncClient, you must specify url, region and auth properties on the config object or a custom link.'
-      );
-    }
-
     const inMemoryCache = new InMemoryCache(cacheOptions);
-
-    const cache: ApolloCache<NormalizedCacheObject> =
-      customCache || inMemoryCache;
-
-    const link =
-      customLink ||
-      createAppSyncLink({
-        url,
-        region,
-        auth,
-        complexObjectsCredentials,
-      });
+    const httpLink = createHttpLink({ uri: url });
+    const link = ApolloLink.from([
+      createAuthLink({ url, region, auth }),
+      createSubscriptionHandshakeLink(url, httpLink),
+    ]);
 
     const newOptions: ApolloClientOptions<any> = {
       ...options,
       link,
-      cache,
+      cache: inMemoryCache,
     };
 
     super(newOptions);
   }
 }
-
-export type QuerySyncOptions<T, TVariables = OperationVariables> = {
-  query: DocumentNode;
-  variables: TVariables;
-  update: MutationUpdaterFn<T>;
-};
-
-export type BaseQuerySyncOptions<
-  T,
-  TVariables = OperationVariables
-> = QuerySyncOptions<T, TVariables> & {
-  baseRefreshIntervalInSeconds?: number;
-};
-
-export type SubscribeWithSyncOptions<T, TVariables = OperationVariables> = {
-  baseQuery?: BaseQuerySyncOptions<T, TVariables>;
-  subscriptionQuery?: QuerySyncOptions<T, TVariables>;
-  deltaQuery?: QuerySyncOptions<T, TVariables>;
-};
 
 export default AWSAppSyncClient;
 export { AWSAppSyncClient };
